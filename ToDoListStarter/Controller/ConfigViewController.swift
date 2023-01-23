a4596d1f77016424dccc8127a490fc25c6af1341
raul oropeza cussi <rauloropezacussi@rauls-MacBook-Pro.local>
2023-01-22 23:11:17 -0600

    first commit

---
+++ ToDoListStarter/Controller/ConfigViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConfigViewController.swift
+//  ToDoListStarter
+//
+//  Created by Grisel Angelica Perez Quezada on 29/12/22.
+//
+
+import UIKit
+
+class ConfigViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
